7e988ef172865bfd6f27a28a0d3dda9adac7c6af
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-11-22 22:00:19 -0700

    adds basic questions for employee class

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 const { prompt } = require("inquirer");
-
+//gonna store employee objects and push user inputs into
 employeeArr = [];
 
 loop = true;
 const start = async () => {
   while (loop) {
-    input = await prompt([
+    startPrompt = await prompt([
       {
         type: "confirm",
         name: "continue",
         message: "Would you like to start the program?",
       },
     ]);
-    if (!input.continue) {
+    if (!startPrompt.continue) {
       loop = false;
+      return;
     }
+    userInput = await prompt([
+      {
+        name: "employeeName",
+        type: "input",
+        message: "Whats the employees name?",
+      },
+      {
+        name: "employeeId",
+        type: "input",
+        message: "Whats the employees ID?",
+      },
+      {
+        name: "employeeEmail",
+        type: "input",
+        message: "Whats the employees Email?",
+      },
+      {
+        name: "employeeJob",
+        type: "list",
+        message: "Whats their Job?",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+    ]);
+    console.log(userInput);
   }
 };
 
